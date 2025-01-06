da40bb5bad1d56c4d6c9328aeae3473c7f3a5888
Carmona <lecape27@gmail.com>
2025-01-06 03:29:25 -0600

    CU terminados

--- lib/presentation/screens/videogame_details/videogame_details_view.dart
+++ lib/presentation/screens/videogame_details/videogame_details_view.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; 
+import 'package:intl/intl.dart';
 import 'package:ivdb/domain/entities/user_entity.dart';
+import 'package:ivdb/presentation/widgets/show_comments/comment_card_box.dart';
+import 'package:ivdb/domain/usecases/show_comments_usecase.dart';
 
 class VideogameDetailsView extends HookConsumerWidget {
   const VideogameDetailsView({
@@ -17,7 +19,7 @@ class VideogameDetailsView extends HookConsumerWidget {
     required this.developers,
     required this.genres,
     required this.description,
-    required this.user
+    required this.user,
   });
 
   final UserEntity user;
@@ -38,6 +40,11 @@ class VideogameDetailsView extends HookConsumerWidget {
     final DateFormat dateFormat = DateFormat('dd/MM/yyyy');
     final String formattedDate = dateFormat.format(releaseDate);
 
+    // Obtener comentarios
+    final showCommentsUseCase = ref.watch(showCommentsUseCaseProvider);
+
+    final int? sessionRole = user.roleId;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -49,6 +56,7 @@ class VideogameDetailsView extends HookConsumerWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Imagen y detalles básicos
               Center(
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
@@ -78,52 +86,28 @@ class VideogameDetailsView extends HookConsumerWidget {
               ),
               const SizedBox(height: 10),
               Text(
-                'Descripcion: ',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Text(
-                  description,
-                  style: TextStyle(fontSize: 16),
-                ),
+                'Descripcion: $description',
+                style: const TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 10),
               Text(
-                'Generos: ',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Text(
-                  genres,
-                  style: TextStyle(fontSize: 16),
-                )
+                'Generos: $genres',
+                style: const TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 10),
               Text(
-                'Plataformas: ',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Text(
-                  platforms,
-                  style: TextStyle(fontSize: 16),
-                )
+                'Plataformas: $platforms',
+                //user.roleId.toString(),
+                style: const TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 10),
               Row(
                 children: [
                   const Text(
                     'Desarrolladora: ',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(width: 10),
-                  Text(
-                    developers,
-                    style: TextStyle(fontSize: 16)
-                  )
+                  Text(developers, style: const TextStyle(fontSize: 16)),
                 ],
               ),
               const SizedBox(height: 10),
@@ -131,13 +115,9 @@ class VideogameDetailsView extends HookConsumerWidget {
                 children: [
                   const Text(
                     'Fecha de lanzamiento: ',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(width: 10),
-                  Text(
-                    formattedDate,
-                    style: TextStyle(fontSize: 16)
-                  )
+                  Text(formattedDate, style: const TextStyle(fontSize: 16)),
                 ],
               ),
               const SizedBox(height: 10),
@@ -167,6 +147,117 @@ class VideogameDetailsView extends HookConsumerWidget {
                     style: const TextStyle(fontSize: 16),
                   ),
                 ],
+              ),
+
+              //Boton Cardone
+              
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: sessionRole == 1 ?
+                TextButton(onPressed: (){
+                  print("Boton eliminar videojuego pulsado");
+                },
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  backgroundColor: const Color(0xff1971c2),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                ),
+                child: const Text(
+                  "Eliminar videojuego",
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                )
+                :const SizedBox.shrink(),
+              ),
+              // Mostrar comentarios
+              const SizedBox(height: 20),
+              FutureBuilder(
+                future: showCommentsUseCase.call(title, releaseDate),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        'Error al obtener los comentarios: ${snapshot.error}',
+                        style: const TextStyle(color: Colors.amber),
+                      ),
+                    );
+                  } else if (snapshot.hasData) {
+                    final result = snapshot.data;
+                    return result!.fold(
+                      (fail) => Center(
+                        child: Text(
+                          'Error: ${fail.message}',
+                          style: const TextStyle(color: Colors.amber),
+                        ),
+                      ),
+                      (comments) {
+                        final criticComments = comments[0];
+                        final publicComments = comments[1];
+
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Comentarios de los críticos:',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff1971c2),
+                              ),
+                            ),
+                            if (criticComments.isEmpty)
+                              const Text(
+                                'Aun no existen comentarios de los criticos.',
+                                style: TextStyle(fontSize: 16),
+                              )
+                            else
+                              ...criticComments.map(
+                                (comment) => CommentCardBox(comment: comment,
+                                sessionRole: user.roleId!.toInt(),
+                                releaseDate: releaseDate,
+                                title: title
+                                ),
+                              ),
+                            const SizedBox(height: 20),
+                            const Text(
+                              'Comentarios del público:',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff1971c2),
+                              ),
+                            ),
+                            if (publicComments.isEmpty)
+                              const Text(
+                                'Aun no existen comentarios del publico.',
+                                style: TextStyle(fontSize: 16),
+                              )
+                            else
+                              ...publicComments.map(
+                                (comment) => CommentCardBox(comment: comment,
+                                sessionRole: user.roleId!.toInt(),
+                                releaseDate: releaseDate,
+                                title: title
+                                ),
+                              ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: Text(
+                        'No hay comentarios disponibles.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    );
+                  }
+                },
               ),
             ],
           ),
